b724587e407429cd0b2d8d1fc0bb49da90e84321
zKazemeini125 <kazemeiniz125@gmail.com>
2024-10-28 13:13:01 +0330

    Set initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {  Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Grid templateAreas={`"navbar navbar"
+      "sideMenu main" `}
+      gap={1}>
+        <GridItem bg={"red"} area={"navbar"}> navbar</GridItem>
+        <GridItem bg={"orange"} area={"sideMenu"}>sideMenu</GridItem>
+        <GridItem bg={"blue"} area={"main"}>maine</GridItem>
+
+    </Grid>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -9,5 +8,5 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider>
      <App />
     </ChakraProvider>
-  </StrictMode>,
+  </StrictMode>
 )
