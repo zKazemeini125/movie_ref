86d988060f2d1c76c7756f39378675617093fb59
zKazemeini125 <kazemeiniz125@gmail.com>
2024-11-01 23:21:17 +0330

    Create Movie-grid component

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-Client";
 import { IFetchMoviesList, IMovie } from "../Interface";
-import { Image } from "@chakra-ui/react";
+import { Image, Text } from "@chakra-ui/react";
+import { HiOutlineEllipsisHorizontalCircle } from "react-icons/hi2";
 
 const MovieGrid = () => {
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    apiClient.get<IFetchMoviesList>("movies").then((res) => {
-      setMovies(res.data.data);
-      console.log(movies);
-    }).catch(err=>setError(err));
-  },[]);
+    apiClient
+      .get<IFetchMoviesList>("movies")
+      .then((res) => {
+        setMovies(res.data.data);
+        console.log(movies);
+      })
+      .catch((err) => setError(err.message));
+  }, []);
   return (
     <>
+      {error && <Text>{error}</Text>}
       <ul>
         {movies.map((m) => {
-          return<>
-                 <li key={m.id}>{m.title}</li>
-                 <Image src={m.images[2]}></Image>
-          </>
-   })}
+          return (
+            <>
+              <li key={m.id}>{m.title}</li>
+              <Image src={m.images[2]}></Image>
+            </>
+          );
+        })}
       </ul>
     </>
   );
